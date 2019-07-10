775415042f1579187a5a68d7e396fc86cb979fec
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-11 09:52:42 +1000

    copy bot file

--- bot.js
+++ bot.js
@@ -7,9 +7,10 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+	console.log(message);
 	// author = message.author
-	console.log(message.author);
-	message.channel.send(`@${message.author.username}#${message.author.discriminator} you are a fuck`)
+	// console.log(message.author);
+	// message.channel.send(`@${message.author.username}#${message.author.discriminator} you are a fuck`)
   // if (message.content.match(/meme/i)) {
   //   message.channel.send('sick post my dude')
   // }
