0c72e9ce19afcac85f802b96a3452f0cb886c43d
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-22 13:49:49 +1000

    cors

--- index.js
+++ index.js
@@ -28,21 +28,20 @@ const app = express()
 //
 // }
 
-// var whitelist = ['https://elated-lovelace-d9b735.netlify.com', 'https://supermarche-croissant-13272.herokuapp.com/', 'https://stormy-tundra-35633.herokuapp.com/', 'https://discordapp.com' ]
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+var whitelist = ['https://elated-lovelace-d9b735.netlify.com', 'https://supermarche-croissant-13272.herokuapp.com/', 'https://stormy-tundra-35633.herokuapp.com/', 'https://discordapp.com' ]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 
 
-app.use(cors())
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 
 app.use('/api/discord', require('./api/discord'));
